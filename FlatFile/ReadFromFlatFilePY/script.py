144e2ff98683f807eb3b55f28b53ccadbab33cc7
Vivek <vivekaxl@gmail.com>
2016-10-24 20:46:12 -0400

    finished scripts

--- FlatFile/ReadFromFlatFilePY/script.py
+++ FlatFile/ReadFromFlatFilePY/script.py
@@ -1,17 +1,22 @@
-import sys
-def get_csv(textaslist):
+
+def get_csv(textaslist, norows):
     content = textaslist
     extracted_content = []
     index = 0
-    while index <= len(content):
-        print index, content[index], sys.getsizeof(content[index])
-        index += 1
-        import pdb
-        pdb.set_trace()
-        raw_input()
+    temp_content = []
+    while index < len(content):
+        first_four_bytes = bytearray(content[index:index+4])
+        print first_four_bytes[0]
+        data_start = index+4
+        data_length = first_four_bytes[0]
+        data = content[data_start:data_start+data_length]
+        index = data_start+data_length
+        temp_content.append(data)
+        if len(temp_content) == norows:
+            extracted_content.append(temp_content)
+            temp_content = []
 
 
-
-if  __name__ == "__main__":
+if __name__ == "__main__":
     content = open("./Data/c_ecolids_regular_csv.txt._1_of_2").read()
-    get_csv(content)
+    get_csv(content, 9)
